73bbb0022e8ea4c79a2c9b811b80d94ca5eeb480
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-02-12 12:07:06 +1300

    edit readme.md

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-pandoc -t revealjs -s -o index.html slides.md -V revealjs-url=https://unpkg.com/reveal.js/ --include-in-header=slides.css -V theme=solarized
+pandoc -t revealjs -s -o index.html README.md -V revealjs-url=https://unpkg.com/reveal.js/ --include-in-header=slides.css -V theme=solarized
